d707d255460be96aa2543cb210457a1eb0c0565a
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-04-27 18:29:15 -0400

    updated create account to show server message and full width buttons

--- src/screens/CreateAccount.svelte
+++ src/screens/CreateAccount.svelte
@@ -7,17 +7,18 @@ import Alert from '../components/Alert.svelte';
 let username = '';
 let password = '';
 let passwordConfirm = '';
-let errorMessage = '';
+let serverMessage = '';
 let isCreated = false;
 
 $: usernameIsValid = /^\w{3}\w*$/.test(username);
 $: passwordIsValid = /^\S{3}\S*$/.test(password) && password === passwordConfirm;
 
 const handleSubmit = () => {
-    createAccount(username, password).then(() => {
+    createAccount(username, password).then(({ message }) => {
         isCreated = true;
+        serverMessage = message;
     }, (error) => {
-        errorMessage = error;
+        serverMessage = error;
     });
 };
 
@@ -29,14 +30,14 @@ const handleLogin = () => {
 </script>
 <article>
     <h2>Create Account</h2>
-    {#if errorMessage}
-        <Alert isError message={errorMessage} />
-    {/if}
     {#if !isCreated}
+        {#if serverMessage}
+            <Alert isError message={serverMessage} />
+        {/if}
         <form on:submit|preventDefault={handleSubmit}>
             <div>
                 <label for="username">Username</label>
-                <input on:input={errorMessage = ''} bind:value={username} id="username" type="text">
+                <input on:input={serverMessage = ''} bind:value={username} id="username" type="text">
             </div>
             <div>
                 <label for="password">Password</label>
@@ -52,12 +53,14 @@ const handleLogin = () => {
         </form>
         <a href={loginHash} class="link">Login</a>
     {:else}
-        <Alert message="Account Created!" />
-        <div>
-            <button on:click={handleLogin} class="button">Login</button>
-        </div>
-        <div>
-            <a href={homeHash} class="button alt">Home</a>
+        <Alert message={serverMessage} />
+        <div class="next-steps">
+            <div>
+                <button on:click={handleLogin} class="button">Login</button>
+            </div>
+            <div>
+                <a href={homeHash} class="button alt">Home</a>
+            </div>
         </div>
     {/if}
 </article>
@@ -78,7 +81,8 @@ div {
     margin-bottom: var(--spacing-md);
 }
 
-form {
+form,
+.next-steps {
     max-width: 100%;
     width: 400px;
 }
