51d55a26c24ccdb10ca71dd1245492feac9d0ac0
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-05-01 15:43:01 -0400

    added redirect for invalid subscription id

--- src/screens/EditSubscription.svelte
+++ src/screens/EditSubscription.svelte
@@ -2,11 +2,18 @@
 import { onMount } from 'svelte';
 import { subscriptions } from '../utils/store';
 import { getSubscriptions } from '../utils/http';
-import { editSubscriptionPattern } from '../utils/routing';
+import { editSubscriptionPattern, dashboardHash } from '../utils/routing';
 import CreateSubscription from './CreateSubscription.svelte';
 
-const [editId] = editSubscriptionPattern.getParams();
+let [editId] = editSubscriptionPattern.getParams();
 $: subscription = $subscriptions ? $subscriptions.find(({ id }) => id === editId) : null;
+$: if ($subscriptions && !subscription) {
+    window.location.hash = dashboardHash;
+}
+
+const initialize = () => {
+    [editId] = editSubscriptionPattern.getParams()
+};
 
 onMount(() => {
     if (!$subscriptions) {
@@ -16,4 +23,5 @@ onMount(() => {
     }
 });
 </script>
+<svelte:window on:hashchange={initialize} />
 <CreateSubscription isEdit={true} {subscription} />
