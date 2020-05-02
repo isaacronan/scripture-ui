eacdb1a848a836929c3b3543892b5db81db8ceae
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-05-01 21:47:17 -0400

    added bounds for numeric input buttons

--- src/components/NumericInput.svelte
+++ src/components/NumericInput.svelte
@@ -1,27 +1,29 @@
 <script>
 import { createEventDispatcher } from 'svelte';
 export let value = 0;
+export let min = 1;
+export let max = 10;
 
 const dispatch = createEventDispatcher();
 
 const handleDecrement = () => {
-    value -= 1;
-    dispatch('change', { value });
+    dispatch('change', { value: value - 1 });
 };
 
 const handleIncrement = () => {
-    value += 1;
-    dispatch('change', { value });
+    dispatch('change', { value: value + 1 });
 };
 
-const handleBlur = () => {
-    dispatch('change', { value });
+const handleInput = (event) => {
+    if (event.target.value !== '') {
+        dispatch('change', { value: Math.floor(event.target.value )});
+    }
 };
 </script>
 <div>
-    <button on:click={handleDecrement} class="button decrement"><i class="fas fa-minus" /></button>
-    <input type="number" on:blur={handleBlur} bind:value>
-    <button on:click={handleIncrement} class="button increment"><i class="fas fa-plus" /></button>
+    <button disabled={value <= min} on:click={handleDecrement} class="button decrement"><i class="fas fa-minus" /></button>
+    <input type="number" on:input={handleInput} {value}>
+    <button disabled={max !== null && value >= max} on:click={handleIncrement} class="button increment"><i class="fas fa-plus" /></button>
 </div>
 <style>
 div {
