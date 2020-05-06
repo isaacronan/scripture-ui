76abfeafa970ee936c227490c0917aadfd4d8d98
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-05-06 10:28:14 -0400

    added warning message for no subscriptions; updated layout for dashboard buttons

--- src/screens/Dashboard.svelte
+++ src/screens/Dashboard.svelte
@@ -4,6 +4,7 @@ import { editSubscriptionHash, createSubscriptionHash, issueHash, resetHash, del
 import { subscriptions } from '../utils/store';
 import PatientContainer from '../components/PatientContainer.svelte';
 import ListItem from '../components/ListItem.svelte';
+import Alert from '../components/Alert.svelte';
 import { onMount } from 'svelte';
 
 onMount(() => {
@@ -24,11 +25,19 @@ onMount(() => {
     <section>
         <h2>Subscriptions</h2>
         <PatientContainer isDark={true} isWaiting={!$subscriptions}>
-            <ul class="grid-list">
-                {#each $subscriptions as subscription}
-                    <ListItem primaryHref={editSubscriptionHash(subscription.id)} destinationHref={issueHash(subscription.id)} title={subscription.name} />
-                {/each}
-            </ul>
+            {#if $subscriptions.length}
+                <ul class="grid-list">
+                    {#each $subscriptions as subscription}
+                        <ListItem primaryHref={editSubscriptionHash(subscription.id)} destinationHref={issueHash(subscription.id)} title={subscription.name} />
+                    {/each}
+                </ul>
+            {:else}
+                <div class="flex-container">
+                    <div>
+                        <Alert isError message="No subscriptions!" />
+                    </div>
+                </div>
+            {/if}
             <div class="new">
                 <a href={createSubscriptionHash} class="plain-button"><i class="fas fa-plus" />New Subscription</a>
             </div>
@@ -36,13 +45,23 @@ onMount(() => {
     </section>
     <section>
         <h2>Account Settings</h2>
-        <a href={resetHash} class="button alt">Reset Password</a>
-        <a href={deleteHash} class="button alt negative">Delete Account</a>
+        <div class="flex-container">
+            <a href={resetHash} class="button alt">Reset Password</a>
+            <a href={deleteHash} class="button alt negative">Delete Account</a>
+        </div>
     </section>
 </article>
 <style>
 article {
     color: var(--dark);
+}
+
+section + section {
+    margin-top: var(--spacing-md);
+}
+
+h2 {
+    margin-bottom: var(--spacing-sm);
 }
 
 .new {
@@ -58,5 +77,23 @@ article {
 .new i {
     font-size: 2rem;
     margin-right: var(--spacing-md);
+}
+
+.flex-container {
+    display: flex;
+}
+
+.flex-container > * {
+    flex-basis: 100%;
+}
+
+.flex-container a + a {
+    margin-left: var(--spacing-sm);
+}
+
+@media screen and (min-width: 768px) {
+    .flex-container > * {
+        flex-basis: auto;
+    }
 }
 </style>
